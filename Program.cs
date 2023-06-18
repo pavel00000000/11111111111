8b46fac7a21affacffd5cf9c35e85dc570a27b4d
Дейнека Павлик <freddik1990@gmail.com>
2023-06-18 18:52:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿public delegate void MotionDetectedEvent(string location);
+
+class MotionSensor
+{
+    public event MotionDetectedEvent? ChangeConsoleColor;
+    public string Name { get; set; }
+
+    public MotionSensor(string name)
+    {
+        Name = name;
+    }
+
+    public void DetectMotion(string location)
+    {
+        ChangeConsoleColor?.Invoke(location);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"MotionSensor Name: {Name}");
+    }
+}
+
+class Thermostat
+{
+    public event MotionDetectedEvent? ChangeConsoleColor;
+    public string Name { get; set; }
+
+    public Thermostat(string name)
+    {
+        Name = name;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Thermostat Name: {Name}");
+    }
+}
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        MotionSensor motionSensor = new MotionSensor("MotionSensor");
+        motionSensor.ChangeConsoleColor += MotionDetectedEvent;
+        motionSensor.DetectMotion("put");
+
+        Thermostat thermostat = new Thermostat("Thermostat");
+        thermostat.ChangeConsoleColor += MotionDetectedEvent;
+        thermostat.ChangeConsoleColor -= MotionDetectedEvent; // Пример отписки от события
+
+        Console.ReadLine();
+    }
+
+    static void MotionDetectedEvent(string location)
+    {
+        if (location == "put")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else if (location == "take")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+}
